9da49fc54beea76283d31d76e63333648ec2f5cf
Adam Emieljaniuk <beginner1986@gmail.com>
2020-02-04 20:17:09 +0100

    Task 4 done.

--- ModernCpp/main.cpp
+++ ModernCpp/main.cpp
@@ -1,16 +1,44 @@
 #include <iostream>
-#include <numeric>
+
+bool isPrime(const int x);
 
 int main()
 {
-	int x, y;
-	std::cout << "x = ";
-	std::cin >> x;
-	std::cout << "y = ";
-	std::cin >> y;
+	int limit = 0;
+	std::cout << "limit = ";
+	std::cin >> limit;
 
-	// C++ 17 feature
-	std::cout << "lcm(" << x << "," << y << ") = " << std::lcm(x, y) << std::endl;
+	for (int i = limit - 1; i > 1; i--)
+	{
+		if (isPrime(i))
+		{
+			std::cout << "The greatest prime number smaller than " 
+				<< limit << " is " << i << "." << std::endl;
+			break;
+		}
+	}
 
 	return 0;
+}
+
+bool isPrime(const int x)
+{
+	if (x < 2)
+		return false;
+	else if (x == 2)
+		return true;
+	else if(x % 2 == 0 || x % 3 == 0)
+	{
+		return false;
+	}
+	else
+	{
+		for (int i = 5; i * i < x; i += 6)
+		{
+			if (x % i == 0 || x % (i + 2) == 0)
+				return false;
+		}
+	}
+
+	return true;
 }
