793a311c400df8f5a88e1c0e09cf7258e28e3b04
Adam Emieljaniuk <beginner1986@gmail.com>
2020-02-04 19:51:48 +0100

    Task 1 done.

--- ModernCpp/main.cpp
+++ ModernCpp/main.cpp
@@ -2,5 +2,17 @@
 
 int main()
 {
+	int limit;
+	std::cout << "Type the limit: ";
+	std::cin >> limit;
+
+	long long result = 0;
+	for (unsigned int i = 1; i <= limit; i++)
+		if (i % 3 == 0 || i % 5 == 0)
+			result += i;
+
+	std::cout << "The result is " << result << std::endl;
+	std::cin.get();
+
 	return 0;
 }
